1e4ad7b454b2819722c574ec783b25d94ed5d172
Adarsh Mishra <adjee2020@gmail.com>
2023-09-04 15:42:45 +0530

    added async

--- pages/_app.js
+++ pages/_app.js
@@ -11,33 +11,33 @@ const App = ({ Component, pageProps }) => {
         </StateContextProvider>
       </ThirdwebProvider>
 
-      <script src="/js/vendor/jquery.js"></script>
-      <script src="/js/vendor/jquery.nice-select.min.js"></script>
-      <script src="/js/vendor/jquery-ui.js"></script>
-      <script src="/js/vendor/modernizer.min.js"></script>
-      <script src="/js/vendor/feather.min.js"></script>
-      <script src="/js/vendor/slick.min.js"></script>
-      <script src="/js/vendor/bootstrap.min.js"></script>
-      <script src="/js/vendor/sal.min.js"></script>
-      <script src="/js/vendor/particles.js"></script>
-      <script src="/js/vendor/jquery.style.swicher.js"></script>
-      <script src="/js/vendor/js.cookie.js"></script>
-      <script src="/js/vendor/count-down.js"></script>
-      <script src="/js/vendor/isotop.js"></script>
-      <script src="/js/vendor/imageloaded.js"></script>
-      {/* <script src="/js/vendor/backtoTop.js"></script> */}
-      <script src="/js/vendor/odometer.js"></script>
-      <script src="/js/vendor/jquery-appear.js"></script>
-      <script src="/js/vendor/scrolltrigger.js"></script>
-      <script src="/js/vendor/jquery.custom-file-input.js"></script>
-      <script src="/js/vendor/savePopup.js"></script>
-      <script src="/js/vendor/vanilla.tilt.js"></script>
+      <script src="/js/vendor/jquery.js" async></script>
+      <script src="/js/vendor/jquery.nice-select.min.js" async></script>
+      <script src="/js/vendor/jquery-ui.js" async></script>
+      <script src="/js/vendor/modernizer.min.js" async></script>
+      <script src="/js/vendor/feather.min.js" async></script>
+      <script src="/js/vendor/slick.min.js" async></script>
+      <script src="/js/vendor/bootstrap.min.js" async></script>
+      <script src="/js/vendor/sal.min.js" async></script>
+      <script src="/js/vendor/particles.js" async></script>
+      <script src="/js/vendor/jquery.style.swicher.js" async></script>
+      <script src="/js/vendor/js.cookie.js" async></script>
+      <script src="/js/vendor/count-down.js" async></script>
+      <script src="/js/vendor/isotop.js" async></script>
+      <script src="/js/vendor/imageloaded.js" async></script>
+      {/* <script src="/js/vendor/backtoTop.js" async></script> */}
+      <script src="/js/vendor/odometer.js" async></script>
+      <script src="/js/vendor/jquery-appear.js" async></script>
+      <script src="/js/vendor/scrolltrigger.js" async></script>
+      <script src="/js/vendor/jquery.custom-file-input.js" async></script>
+      <script src="/js/vendor/savePopup.js" async></script>
+      <script src="/js/vendor/vanilla.tilt.js" async></script>
 
-      <script src="/js/main.js"></script>
+      <script src="/js/main.js" async></script>
 
-      {/* <script src="/js/vendor/web3.min.js"></script>
-      <script src="/js/vendor/maralis.js"></script>
-      <script src="/js/vendor/nft.js"></script> */}
+      <script src="/js/vendor/web3.min.js" async></script>
+      <script src="/js/vendor/maralis.js" async></script>
+      <script src="/js/vendor/nft.js" async></script>
     </>
   );
 };
